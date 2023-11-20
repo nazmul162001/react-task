58ec773591a243dd58b52ac01014c254ba5e5d17
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-11-21 01:07:03 +0600

    design dashboard header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AuthProvider from "./authContext";
 import GlobalProvider from "./globalContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./main";
+import "./app.css";
 
 function App() {
   return (
--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { FaRegUser } from "react-icons/fa";
 
 const AdminDashboardPage = () => {
   return (
     <>
-      <div className="w-full flex justify-center items-center text-7xl h-screen text-gray-700 ">
-        Dashboard
-      </div>
+      <section className="w-full bg-[#111111] h-screen text-white py-10 px-5 md:px-10">
+        {/* logo & logout  */}
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-bold">APP</h1>
+          <button className="flex items-center gap-2 text-sm px-5 py-1 text-black rounded-full bg-[#9BFF00]">
+            <span className="">
+              <FaRegUser />
+            </span>
+            <span>Logout</span>
+          </button>
+        </div>
+
+        {/* Dashboard Header  */}
+        <div className="w-full p-5 mt-10">
+          <div className="flex justify-between items-center">
+            <h1 className="text-3xl text-gray-400">Today's leaderboard</h1>
+            <div className="flex justify-between gap-5 items-center bg-[#424141c0] p-5 rounded-lg">
+              {/* date  */}
+              <h4 className="text-gray-400">22 November 2023</h4>
+              <span className="bg-[#9BFF00]  py-1 px-2 text-gray-500 rounded">Submissions Open</span>
+              <h4 className="text-gray-400">11:14</h4>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
