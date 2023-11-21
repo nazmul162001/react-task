70f505c402e4eaa4b6c576ea105975ec7cd500b4
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-11-21 18:00:08 +0600

    fixed pagination

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,102 +6,14 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 
 const VideoCard = () => {
-  const [allItems, setAllItems] = useState([
-    {
-      id: "1",
-      name: "Cryptocurrency Explained: The Future of Money",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "John Smith",
-    },
-    {
-      id: "2",
-      name: "Mastering Algorithms: The Key to Efficient Programming",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Emily Johnson",
-    },
-    {
-      id: "3",
-      name: "The Evolution of AI in Modern Technology",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Michael Williams",
-    },
-    {
-      id: "4",
-      name: "Data Privacy: Navigating the Digital Age",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Olivia Brown",
-    },
-    {
-      id: "5",
-      name: "Future of Transportation: From Hyperloop to Flying Cars",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Daniel Martinez",
-    },
-    {
-      id: "6",
-      name: "Sustainable Energy: Innovations and Challenges",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Sophia Garcia",
-    },
-    {
-      id: "7",
-      name: "Space Exploration: Journey Beyond Our Solar System",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Liam Wilson",
-    },
-    {
-      id: "8",
-      name: "Artificial Intelligence in Healthcare: Revolutionizing Medicine",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Ava Anderson",
-    },
-    {
-      id: "9",
-      name: "Blockchain Technology: Beyond Bitcoin",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Noah Taylor",
-    },
-    {
-      id: "10",
-      name: "Future of Work: Adapting to Remote Employment",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Isabella Clark",
-    },
-    {
-      id: "11",
-      name: "Cybersecurity: Protecting Your Digital Life",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "William Rodriguez",
-    },
-    {
-      id: "12",
-      name: "Virtual Reality: Changing How We Experience Entertainment",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Sophie Moore",
-    },
-    {
-      id: "13",
-      name: "The Ethics of Biotechnology: Balancing Innovation and Responsibility",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Ethan Thompson",
-    },
-    {
-      id: "14",
-      name: "Gaming Industry Trends: From eSports to Game Streaming",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Avery Lee",
-    },
-    {
-      id: "15",
-      name: "Augmented Reality: Enhancing the Real World with Technology",
-      like: Math.floor(Math.random() * 50) + 50,
-      author: "Charlotte Hill",
-    },
-  ]);
+  const [allItems, setAllItems] = useState([]);
+  const [availableVideoData, setAvailableVideoData] = useState([]);
+  // console.log(availableVideoData?.length);
 
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
 
   const [currentPage, setCurrentPage] = useState(1);
+  console.log(currentPage);
   const itemsPerPage = 10;
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -119,11 +31,11 @@ const VideoCard = () => {
   };
 
   const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
+    setCurrentPage(currentPage + 1);
   };
 
   const handlePrevPage = () => {
-    setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 1));
+    setCurrentPage(currentPage - 1);
   };
 
   useEffect(() => {
@@ -152,6 +64,8 @@ const VideoCard = () => {
         if (response.ok) {
           const data = await response.json();
           setAllItems(data.list);
+          setAllItems((prevItems) => [...prevItems, ...data.list]);
+
           // console.log(data.list); // Log the fetched video data
         } else {
           throw new Error("Failed to fetch");
@@ -184,7 +98,9 @@ const VideoCard = () => {
       </section>
       <div className="px-5 my-8 flex justify-end gap-3">
         <button
-          className="px-5 py-1 bg-gray-700 rounded-lg"
+          className={`px-5 py-1 bg-gray-700 rounded-lg ${
+            currentPage === 1 ? "cursor-not-allowed" : ""
+          }`}
           onClick={handlePrevPage}
           disabled={currentPage === 1}
         >
@@ -195,7 +111,7 @@ const VideoCard = () => {
         <button
           className="px-5 py-1 bg-gray-700 rounded-lg"
           onClick={handleNextPage}
-          disabled={indexOfLastItem >= allItems.length}
+          // disabled={indexOfLastItem >= allItems.length}
         >
           <span>
             <FaLongArrowAltRight />
